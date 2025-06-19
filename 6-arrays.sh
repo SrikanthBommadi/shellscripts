9b30f82a9ceda6302a11a3cb81eb52909393731d
SrikanthBommadi <bommadisrikanth1@gmail.com>
2025-06-19 09:57:27 -0400

    shell

--- 6-arrays.sh
+++ 6-arrays.sh
@@ -6,4 +6,4 @@ echo "First Movie: ${MOVIES[0]}"
 echo "First Movie: ${MOVIES[3]}"
 echo "First Movie: ${MOVIES[4]}"
 
-echo "All movies: ${MOVIES[@]}"
+echo "All movies: ${MOVIES[@]}" ## @ means alla of them
