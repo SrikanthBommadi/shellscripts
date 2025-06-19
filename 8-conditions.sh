c100a5ce1a8e58edb8124b19dee8a4092d1acc90
SrikanthBommadi <bommadisrikanth1@gmail.com>
2025-06-19 08:55:42 -0400

    shell

--- 8-conditions.sh
+++ 8-conditions.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-# -gt =greater than
-# -lt = less than
-# -eq = equal to
+NUMBER=$1
+
+# -gt --> greater than
+# -lt --> less than
+# -eq --> equal
+# -ne --> not equal
+
+if [ $NUMBER -lt 10 ]
+then
+    echo "Given number $NUMBER is less than 10"
+else
+    echo "Given number $NUMBER is not less than 10"
+fi # is "if" spelled backward — it tells the shell where the if block ends.
